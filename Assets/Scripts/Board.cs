6f3281c02faedffbdaaf3021e58a04e98df659bd
RadoslawGarlicki <rg057159@student.ath.edu.pl>
2023-04-01 22:46:26 +0200

    idk part 2 (finished without a match)

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -23,38 +23,63 @@ public class Board : MonoBehaviour
     //metoda ustawiaj¹ca planszê na pocz¹tku gry
     private void SetUp()
     {
-        for (int i = 0; i < width; i++)            // nie zmieniaæ kolejnoœci height i width, bo kurwa dzia³a
+        for (int i = 0; i < height; i++)
         {
-            for(int j = 0; j < height; j++)          // nie zmieniaæ kolejnoœci height i width, bo kurwa dzia³a
+            for(int j = 0; j < width; j++)
             {
                 Vector2 tempPosition = new Vector2(i, j);
                 GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
                 backgroundTile.transform.parent = this.transform; 
                 backgroundTile.name = "( " + i + "," + j + " )";   
-                
                 int dotToUse = Random.Range(0, dots.Length);
-                while (MatchesAt(i,j, dots[dotToUse]))
+
+                int maxIteration = 0;
+                while ((MatchesAt(i,j, dots[dotToUse]) && maxIteration < 100))
                 {
                     dotToUse = Random.Range(0, dots.Length);
+                    maxIteration++;
+                    Debug.Log(maxIteration);
                 }
+                maxIteration = 0;
                 GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
                 dot.transform.parent = this.transform;
                 dot.name = "( " + i + "," + j + " )";
-                allDots[i,j] = dot;                 // tutaj to samo kurwa nie zmieniaæ xD
+                allDots[j,i] = dot;
             }
         }
     }
 
-    private bool MatchesAt(int column, int row, GameObject piece)
+    private bool MatchesAt(int row, int column, GameObject piece)
     {
         if(column > 1 && row > 1)
         {
             if (allDots[column - 1, row].tag == piece.tag && allDots[column - 2, row].tag == piece.tag)
-                return true;
+            { 
+                return true; 
+            }
 
             if (allDots[column, row - 1].tag == piece.tag && allDots[column, row - 2].tag == piece.tag)
+            {
                 return true;
+            }
 
+        }
+        else if(column <= 1 || row <= 1)
+        {
+            if(row > 1)
+            {
+                if (allDots[column, row - 1].tag == piece.tag && allDots[column, row - 2].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+            if (column > 1)
+            {
+                if (allDots[column - 1, row].tag == piece.tag && allDots[column - 2, row].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
         }
 
         return false;
