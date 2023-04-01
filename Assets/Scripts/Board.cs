039e2ef46b7065661b333f609fda1a0caaa5a32e
RadoslawGarlicki <rg057159@student.ath.edu.pl>
2023-04-01 16:58:25 +0200

    repairs v3

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -23,9 +23,9 @@ public class Board : MonoBehaviour
     //metoda ustawiaj¹ca planszê na pocz¹tku gry
     private void SetUp()
     {
-        for (int i = 0; i < height; i++)
+        for (int i = 0; i < height; i++)            // nie zmieniaæ kolejnoœci height i width, bo kurwa dzia³a
         {
-            for(int j = 0; j < width; j++)
+            for(int j = 0; j < width; j++)          // nie zmieniaæ kolejnoœci height i width, bo kurwa dzia³a
             {
                 Vector2 tempPosition = new Vector2(i, j);
                 GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
@@ -35,7 +35,7 @@ public class Board : MonoBehaviour
                 GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
                 dot.transform.parent = this.transform;
                 dot.name = "( " + i + "," + j + " )";
-                allDots[j,i] = dot;
+                allDots[j,i] = dot;                 // tutaj to samo kurwa nie zmieniaæ xD
             }
         }
     }
